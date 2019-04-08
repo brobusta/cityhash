08da81d1f68c7105f991761519952cfc52761315
Vu Thanh Cong <vuthanhcong.ict@gmail.com>
2019-04-08 17:55:29 +0700

    correct behavior when null or empty as input

--- cityhash.go
+++ cityhash.go
@@ -11,5 +11,8 @@ import (
 )
 
 func CityHash64(s []byte) uint64 {
+  if len(s) == 0 {
+    return uint64(C.cityhash64((*C.uint8_t)(C.NULL), 0))
+  }
   return uint64(C.cityhash64((*C.uint8_t)(unsafe.Pointer(&s[0])), C.size_t(len(s))))
 }
